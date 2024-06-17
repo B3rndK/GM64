8ea487c94f0d492ff6fa8621d3c699583138bc44
Bernd Krekeler <berndk@spbk.de>
2024-06-17 15:37:30 +0200

    CPU test

--- src/VIC6569/src/VIC6569.v
+++ src/VIC6569/src/VIC6569.v
@@ -8,13 +8,17 @@
     MOS 6569, "VIC-II"
 */
 
-module VIC6569(input clk, 
+module VIC6569(input clk,
+               input clkDot, 
                input reset, 
+               output clkPhi0,
                output o_hsync, 
                output o_vsync, 
                output [3:0] o_red, 
                output [3:0] o_green, 
-               output [3:0] o_blue);
+               output [3:0] o_blue,
+               input [3:0] debugValue);
+               
 
   reg [3:0] red;
   reg [3:0] green;
@@ -23,6 +27,10 @@ module VIC6569(input clk,
   wire display_on;
   wire [9:0] o_hpos;
   wire [9:0] o_vpos;
+
+  // Creating Phi0 clock for CPU by dividing clkDot by 
+  reg [4:0] cntPhi0;
+  assign clkPhi0=cntPhi0>7;
 
   syncGen sync_gen(
     .clk(clk),
@@ -42,6 +50,16 @@ module VIC6569(input clk,
       blue=0;       
     end 
     else begin
+      case (debugValue)
+        4'b0000 : begin red=0; green=0; blue=0; end
+        4'b0001 : begin red=15;green=0;blue=0; end
+        4'b0010 : begin red=0;green=15;blue=0; end
+        4'b0011 : begin red=15;green=15;blue=0; end
+        4'b0100 : begin red=0;green=15;blue=15; end
+        4'b0101 : begin red=0;green=0;blue=15; end
+        default : begin red=15;green=15;blue=15; end
+      endcase
+      /*
       if (o_vpos>=0 && o_vpos<556) begin
         red=o_vpos % 15;
         green=o_vpos % 15;       
@@ -51,14 +69,28 @@ module VIC6569(input clk,
         red=o_vpos % 15;
         green=0;       
         blue=0;       
-      end
+      end*/
+
     end
   end
 
-  assign  o_red = display_on ? red : 0;
-  assign  o_green = display_on ? green : 0;
-  assign  o_blue = display_on ? blue  : 0;
-  
+  assign  o_red = (display_on) ? red : 0;
+  assign  o_green = (display_on) ? green : 0;
+  assign  o_blue = (display_on) ? blue  : 0;
+
+  // The VIC-II generates the CPU clk (phi0) by dividing the clkDot by 8.
+  always @(posedge clkDot or posedge reset)
+  begin
+    if (reset) begin
+      cntPhi0=0;
+    end 
+    else begin
+      cntPhi0++;
+      if (cntPhi0>16) begin
+        cntPhi0=0;
+      end
+    end
+  end
 endmodule
 
 `endif
